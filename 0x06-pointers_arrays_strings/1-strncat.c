8c6bc6484a5f775b8ca020d2d052ef9166f93360
kenneth <malgaskenneth3@gmail.com>
2022-10-27 13:22:08 -0700

    files

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+
+
 /**
  * _strncat - a function that concatenates two strings
  * @dest:copy to
@@ -7,19 +9,35 @@
  * @n: n
  * Return: Always 0 (Success)
  */
+
 char *_strncat(char *dest, char *src, int n)
+
 {
+
 	int i, co;
 
+
+
 	for (i = 0; dest[i] != '\0'; i++)
+
 	{
+
 	}
 
+
+
 	for (co = 0; co < n; co++)
+
 	{
+
 		dest[i + co] = src[co];
+
 		if (src[co] == '\0')
+
 			co = n;
+
 	}
+
 	return (dest);
+
 }
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+
+
 /**
  * _strncpy - a function that copies a string
  * @dest: copy to
@@ -7,15 +9,27 @@
  * @n: input number of char
  * Return: Always 0 (Success)
  */
+
 char *_strncpy(char *dest, char *src, int n)
+
 {
+
 	int i;
 
+
+
 	for (i = 0; src[i] != '\0'; i++)
+
 		if (i < n)
+
 			dest[i] = src[i];
+
 	while (i < n)
+
 		dest[i++] = '\0';
 
+
+
 	return (dest);
+
 }
