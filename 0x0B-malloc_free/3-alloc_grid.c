c0c2963b919810cb662873f505c999e09cf42b82
kxnnxthmxlgxs <73911764+kxnnxthmxlgxs@users.noreply.github.com>
2022-11-11 02:28:05 +0200

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,47 +1,52 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
-  * str_concat - Concatenates two strings of any size
-  * @s1: the first string to concatenate
-  * @s2: the second string to concatenate
+  * alloc_grid - ...
+  * @width: ...
+  * @height: ...
   *
-  * Return: the two strings concatenated
+  * Return: ...
   */
-
-char *str_concat(char *s1, char *s2)
-
+int **alloc_grid(int width, int height)
 {
+	int i, j, k, l;
+	int **a;
 
-	int i = 0, j = 0, k = 0, l = 0;
-
-	char *s;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	l = i + j;
-	s = malloc((sizeof(char) * l) + 1);
-	if (s == NULL)
+	if (width <= 0 || height <= 0)
 		return (NULL);
-	j = 0;
-	while (k < l)
+
+	a = malloc(sizeof(int *) * height);
+
+	if (a == NULL)
 	{
-		if (k <= i)
-			s[k] = s1[k];
-		if (k >= i)
-		{
-			s[k] = s2[j];
-			j++;
-		}
-		k++;
+		free(a);
+		return (NULL);
 	}
-	s[k] = '\0';
-	return (s);
+
+	for (i = 0; i < height; i++)
+	{
+		a[i] = malloc(sizeof(int) * width);
+
+		if (a[i] == NULL)
+		{
+			for (j = i; j >= 0; j--)
+			{
+				free(a[j]);
+			}
+
+			free(a);
+			return (NULL);
+		}
+	}
+
+	for (k = 0; k < height; k++)
+	{
+		for (l = 0; l < width; l++)
+		{
+			a[k][l] = 0;
+		}
+	}
+
+	return (a);
 }
